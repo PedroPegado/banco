2443914010b37fa41d647622f2cb89177ce63c61
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-12-17 18:17:24 -0300

    testando

--- BancoDoDrazil.py
+++ BancoDoDrazil.py
@@ -1,27 +1,47 @@
-path = 'C:\\Users\\20211174010009\\Documents\\aulas\\banco\\Contas.txt'
+path = 'C:\\Users\\saped\\Documents\\GitHub\\banco\\Contas.txt'
 
 with open(path, 'r') as f:
     contas = f.readlines()
 
-print(contas)
+profiles = []
 
 class ContaBancaria:
     def __init__(self, numero):
         self.numero = numero
-
-        if self.numero not in contas:
-            print('Vimos que você não tem uma conta, crie: ')
-            self.titular = input('Digite seu nome: ')
-            self.saldo = '0.0'
-            with open(path, 'w') as f:
-                f.write(self.titular + '\n')
-                f.write(self.numero + '\n')
-                f.write(self.saldo + '\n')
-    
+        cont1 = 0
+        for c in profiles:
+            if c == self.numero:
+                break
+            cont1+=1
+        contaN = contas[cont1].split('/')
+        self.titular = contaN[0]
+        self.saldo = contaN[2]
+        cont = 0
+        while cont != len(contas):
+            contaN = contas[cont].split('/')
+            profiles.append(contaN[1])
+            cont+=1
+        if self.numero not in profiles:
+            self.criarConta()
+            
+    def criarConta(self):
+        self.titular = input('Digite seu nome:')
+        self.saldo = 0
+        with open(path, 'a') as f:
+            f.write(f'{self.titular}/{self.numero}/{self.saldo}\n')
+                
     def depositar(self, valor):
+        cont = 0
+        for c in profiles:
+            if c == self.numero:
+                break
+            cont+=1
+        contaN = contas[cont].split('/')
         self.saldo = float(self.saldo)
         self.saldo += valor
-
-emilly = ContaBancaria('002')
-
+        self.saldo = str(self.saldo)
+        with open(path, 'w') as f:
+            f.write(f'{self.titular}/{self.numero}/{self.saldo}\n')
+        
+pedro = ContaBancaria('007')
 
